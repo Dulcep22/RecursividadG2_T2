4686e73da7036841d2cb1160f450d6ccc0184cb7
Dulcep22 <25030393@itesa.edu,mx>
2026-09-09 14:23:05 -0600

    Version_02: Factorial recursivo

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+        public int CalcularFactorial (int num)
+        {
+            //caso base o condicion de salida
+
+            if (num == 1)
+            {
+                return 1;
+            }
+
+            //Caoso general
+            return num * CalcularFactorial(num - 1);
+        }
+
+
+    }
+}
